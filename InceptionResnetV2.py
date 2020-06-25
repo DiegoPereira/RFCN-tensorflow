53a942b965200d12cf1eb38231a86d1a7bd4328e
diego pereira <diego.lima@virtus.ufcg.edu.br>
2020-06-25 11:46:39 -0300

    update all variable scope to tf2

--- InceptionResnetV2.py
+++ InceptionResnetV2.py
@@ -25,13 +25,13 @@ class InceptionResnetV2:
 	@staticmethod
 	def block35(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
 		"""Builds the 35x35 resnet block."""
-		with tf.variable_scope(scope, 'Block35', [net], reuse=tf.compat.v1.AUTO_REUSE):
-			with tf.variable_scope('Branch_0'):
+		with tf.compat.v1.variable_scope(scope, 'Block35', [net], reuse=tf.compat.v1.AUTO_REUSE):
+			with tf.compat.v1.variable_scope('Branch_0'):
 				tower_conv = slim.conv2d(net, 32, 1, scope='Conv2d_1x1')
-			with tf.variable_scope('Branch_1'):
+			with tf.compat.v1.variable_scope('Branch_1'):
 				tower_conv1_0 = slim.conv2d(net, 32, 1, scope='Conv2d_0a_1x1')
 				tower_conv1_1 = slim.conv2d(tower_conv1_0, 32, 3, scope='Conv2d_0b_3x3')
-			with tf.variable_scope('Branch_2'):
+			with tf.compat.v1.variable_scope('Branch_2'):
 				tower_conv2_0 = slim.conv2d(net, 32, 1, scope='Conv2d_0a_1x1')
 				tower_conv2_1 = slim.conv2d(tower_conv2_0, 48, 3, scope='Conv2d_0b_3x3')
 				tower_conv2_2 = slim.conv2d(tower_conv2_1, 64, 3, scope='Conv2d_0c_3x3')
@@ -45,10 +45,10 @@ class InceptionResnetV2:
 	@staticmethod
 	def block17(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
 		"""Builds the 17x17 resnet block."""
-		with tf.variable_scope(scope, 'Block17', [net], reuse=tf.compat.v1.AUTO_REUSE):
-			with tf.variable_scope('Branch_0'):
+		with tf.compat.v1.variable_scope(scope, 'Block17', [net], reuse=tf.compat.v1.AUTO_REUSE):
+			with tf.compat.v1.variable_scope('Branch_0'):
 				tower_conv = slim.conv2d(net, 192, 1, scope='Conv2d_1x1')
-			with tf.variable_scope('Branch_1'):
+			with tf.compat.v1.variable_scope('Branch_1'):
 				tower_conv1_0 = slim.conv2d(net, 128, 1, scope='Conv2d_0a_1x1')
 				tower_conv1_1 = slim.conv2d(tower_conv1_0, 160, [1, 7], scope='Conv2d_0b_1x7')
 				tower_conv1_2 = slim.conv2d(tower_conv1_1, 192, [7, 1], scope='Conv2d_0c_7x1')
@@ -62,10 +62,10 @@ class InceptionResnetV2:
 	@staticmethod
 	def block8(net, scale=1.0, activation_fn=tf.nn.relu, scope=None, reuse=None):
 		"""Builds the 8x8 resnet block."""
-		with tf.variable_scope(scope, 'Block8', [net], reuse=tf.compat.v1.AUTO_REUSE):
-			with tf.variable_scope('Branch_0'):
+		with tf.compat.v1.variable_scope(scope, 'Block8', [net], reuse=tf.compat.v1.AUTO_REUSE):
+			with tf.compat.v1.variable_scope('Branch_0'):
 				tower_conv = slim.conv2d(net, 192, 1, scope='Conv2d_1x1')
-			with tf.variable_scope('Branch_1'):
+			with tf.compat.v1.variable_scope('Branch_1'):
 				tower_conv1_0 = slim.conv2d(net, 192, 1, scope='Conv2d_0a_1x1')
 				tower_conv1_1 = slim.conv2d(tower_conv1_0, 224, [1, 3], scope='Conv2d_0b_1x3')
 				tower_conv1_2 = slim.conv2d(tower_conv1_1, 256, [3, 1], scope='Conv2d_0c_3x1')
@@ -190,14 +190,14 @@ class InceptionResnetV2:
 
 					# 17 x 17 x 1024
 					beginBlock('Mixed_6a')
-					with tf.variable_scope('Mixed_6a'):
-						with tf.variable_scope('Branch_0'):
+					with tf.compat.v1.variable_scope('Mixed_6a'):
+						with tf.compat.v1.variable_scope('Branch_0'):
 							tower_conv = slim.conv2d(net, 384, 3, stride=2, padding='VALID', scope='Conv2d_1a_3x3')
-						with tf.variable_scope('Branch_1'):
+						with tf.compat.v1.variable_scope('Branch_1'):
 							tower_conv1_0 = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
 							tower_conv1_1 = slim.conv2d(tower_conv1_0, 256, 3, scope='Conv2d_0b_3x3')
 							tower_conv1_2 = slim.conv2d(tower_conv1_1, 384, 3, stride=2, padding='VALID', scope='Conv2d_1a_3x3')
-						with tf.variable_scope('Branch_2'):
+						with tf.compat.v1.variable_scope('Branch_2'):
 							tower_pool = slim.max_pool2d(net, 3, stride=2, padding='VALID', scope='MaxPool_1a_3x3')
 						net = tf.concat([tower_conv, tower_conv1_2, tower_pool], 3)
 					endBlock(net)
@@ -208,18 +208,18 @@ class InceptionResnetV2:
 					endBlock(net, scope=False, name='aux')
 
 					beginBlock('Mixed_7a')
-					with tf.variable_scope('Mixed_7a'):
-						with tf.variable_scope('Branch_0'):
+					with tf.compat.v1.variable_scope('Mixed_7a'):
+						with tf.compat.v1.variable_scope('Branch_0'):
 							tower_conv = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
 							tower_conv_1 = slim.conv2d(tower_conv, 384, 3, stride=2, padding='VALID', scope='Conv2d_1a_3x3')
-						with tf.variable_scope('Branch_1'):
+						with tf.compat.v1.variable_scope('Branch_1'):
 							tower_conv1 = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
 							tower_conv1_1 = slim.conv2d(tower_conv1, 288, 3, stride=2, padding='VALID', scope='Conv2d_1a_3x3')
-						with tf.variable_scope('Branch_2'):
+						with tf.compat.v1.variable_scope('Branch_2'):
 							tower_conv2 = slim.conv2d(net, 256, 1, scope='Conv2d_0a_1x1')
 							tower_conv2_1 = slim.conv2d(tower_conv2, 288, 3, scope='Conv2d_0b_3x3')
 							tower_conv2_2 = slim.conv2d(tower_conv2_1, 320, 3, stride=2, padding='VALID', scope='Conv2d_1a_3x3')
-						with tf.variable_scope('Branch_3'):
+						with tf.compat.v1.variable_scope('Branch_3'):
 							tower_pool = slim.max_pool2d(net, 3, stride=2, padding='VALID', scope='MaxPool_1a_3x3')
 						net = tf.concat([tower_conv_1, tower_conv1_1, tower_conv2_2, tower_pool], 3)
 					endBlock(net)
